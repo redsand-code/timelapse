9d06770761db63e05abc63757a99bbe82e5e607e
Rudy Sandoval <rsandoval1@gmail.com>
2022-12-01 20:52:11 -0600

    add time

--- test_cam.py
+++ test_cam.py
@@ -3,27 +3,36 @@ import datetime
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
- 
+from skimage.metrics import structural_similarity as ssim
+
+
 def test_image():
     # Open an Image
     img = Image.open('images/timelapse/0.jpg')
-    
+
     I1 = ImageDraw.Draw(img)
 
     now = datetime.datetime.now()
     label = now.strftime("%I:%M:%S %p")
-    
+
     ifont = ImageFont.truetype('Helvetica', 40)
-    I1.text((1000, 600), label, font=ifont, fill =(200, 200, 200))
+    I1.text((1000, 600), label, font=ifont, fill=(200, 200, 200))
 
     img.show()
-    #img.save("0.1.jpg")
+    # img.save("0.1.jpg")
+
 
 def get_hour():
     now = datetime.datetime.now()
-    print(now.strftime("%Y-%m-%d"))
+    morning = datetime.time(6, 40)
+    print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
-    return 0
+    tomorrow = now + datetime.timedelta(days=1)
+
+    finish_time = datetime.datetime.combine(tomorrow.date(), morning)
+    print(finish_time.strftime("%Y-%m-%d %H:%M:%S"))
+
+    return
 
 
 def test_cam():
@@ -34,6 +43,8 @@ def test_cam():
         rval, frame = vc.read()
     else:
         rval = False
+
+    last_frame = None
 
     while rval:
         cv2.imshow("preview", frame)
@@ -46,4 +57,11 @@ def test_cam():
     cv2.destroyWindow("preview")
 
 
-test_image()
+def compare():
+    img0 = Image.open('images/timelapse/0.jpg')
+    img1 = Image.open('images/timelapse/1.jpg')
+    simlarityIndex = ssim(img0, img1)
+    print(simlarityIndex)
+
+
+compare()
